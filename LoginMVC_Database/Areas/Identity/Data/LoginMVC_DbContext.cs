3e514f3ee395ec48ec2f28d66f0ed9182f2cd9ee
Paulius <jurgelis.paulius@gmail.com>
2024-01-18 18:09:25 +0200

    Add project files.

---
+++ LoginMVC_Database/Areas/Identity/Data/LoginMVC_DbContext.cs
@@ -0,0 +1,22 @@
+﻿using LoginMVC_Database.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace LoginMVC_Database.Data;
+
+public class LoginMVC_DbContext : IdentityDbContext<LoginMVC_DatabaseUser>
+{
+    public LoginMVC_DbContext(DbContextOptions<LoginMVC_DbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ LoginMVC_Database/Program.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using LoginMVC_Database.Data;
+using LoginMVC_Database.Areas.Identity.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("LoginMVC_DbContextConnection") ?? throw new InvalidOperationException("Connection string 'LoginMVC_DbContextConnection' not found.");
+
+builder.Services.AddDbContext<LoginMVC_DbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<LoginMVC_DatabaseUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<LoginMVC_DbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
