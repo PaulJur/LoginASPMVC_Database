b4b2dd6bccd1550c0ef1b126b5fbcacf3930d91c
Paulius <jurgelis.paulius@gmail.com>
2024-03-03 17:51:38 +0200

    Edit and Delete
    Added Edit function to edit the fake person data
    
    Added Delete to delete fake person data based on ID
    
    Started working on view to redirect to login if user tries to access the Data view

--- LoginMVC_Database/Controllers/FakePersonController.cs
+++ LoginMVC_Database/Controllers/FakePersonController.cs
@@ -20,7 +20,7 @@ namespace LoginMVC_Database.Controllers
             {
                 try
                 {
-                    
+                
                     return View(objPersonDataList);
                 }
                 catch (Exception ex)
@@ -29,7 +29,7 @@ namespace LoginMVC_Database.Controllers
                 }
             }
 
-            return RedirectToAction("Login", "Account");
+            return RedirectToAction("Error", "Shared");
         }
 
         public IActionResult Create()
@@ -41,7 +41,7 @@ namespace LoginMVC_Database.Controllers
 
         public IActionResult Create(FakePersonData obj)
         {
-            if (_context.FakePeople.Any(n => n.LastName == obj.LastName && n.FirstName == obj.FirstName && n.Age == obj.Age)) //If First name and Last name are the same, give error
+            if (_context.FakePeople.Any(n => n.LastName == obj.LastName && n.FirstName == obj.FirstName && n.Age == obj.Age)) //If First name and Last name and age are the same, give error
             {
                 ModelState.AddModelError("name", "Cannot have an existing person added into the database");
             }
@@ -63,5 +63,67 @@ namespace LoginMVC_Database.Controllers
             }
             return View(obj);//stays on the create category page
         }
+        public IActionResult Edit(int? id)//This is a server side check
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            FakePersonData? FakePersonFromDb = _context.FakePeople.Find(id);//will work off the primary key off the model
+
+            if (FakePersonFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(FakePersonFromDb);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(FakePersonData obj)//This is post http, meaning what the server receives from the user
+        {
+
+            if (ModelState.IsValid)//if everything from FakePersonData.cs is validated like maxlenght and Id, then return valid and execute
+            {
+                _context.FakePeople.Update(obj);//This will Update the category object(made by the user input) to the category table. This keeps track of the changes
+                _context.SaveChanges();//this makes the changes in the database table
+                TempData["success"] = "Category updated succesfully";
+                return RedirectToAction("Data");//This will refresh the database table and show the new stuff
+            }
+            return View(obj);//stays on the create category page
+        }
+
+        public IActionResult Delete(int? id)//This is a server side check
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            FakePersonData? FakePersonFromDb = _context.FakePeople.Find(id);//will work off the primary key off the model
+
+            if (FakePersonFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(FakePersonFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]//this returns the input from the user side to the back-end and the actionName is "Delete"
+        //send data to a server to create/update a resource
+
+        public IActionResult DeletePOST(int? id)
+        {
+            FakePersonData? obj = _context.FakePeople.Find(id);
+
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            _context.FakePeople.Remove(obj);
+            _context.SaveChanges();
+            TempData["success"] = "Category deleted succesfully";
+            return RedirectToAction("Data");
+        }
     }
+
 }
